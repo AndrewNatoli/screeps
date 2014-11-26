e200274c5a0c3ff793a34171cfc2c49d1b4a94f3
Andrew Natoli <andrew@desktopsolutions.com>
2014-11-26 00:23:33 -0500

    Store the type name in a variable so we don't have horrifyingly long calls to look it up

--- population.js
+++ population.js
@@ -4,18 +4,20 @@ var util        = require("util");
 
 module.exports = function() {
     for(var creepType in creepTypes("list")) {
+        
+        var typeName = creepTypes("list")[creepType];
+        
         // Do we not have any of this type?
-        if(util.count(creepTypes("list")[creepType]) === 0) {
+        if(util.count(typeName) === 0) {
             // Do we have a free spawner?
             var spawn = util.spawn();
             if(spawn !== undefined) {
                 // Can we afford this type?
-                if(spawn.energy > creepCosts.getCost(creepTypes(creepTypes("list")[creepType]))) {
-                    console.log("Spawning " + creepTypes("list")[creepType]);
-                    var newName = spawn.createCreep(creepTypes(creepTypes("list")[creepType]));
+                if(spawn.energy > creepCosts.getCost(creepTypes(typeName))) {
+                    var newName = spawn.createCreep(creepTypes(typeName));
                     // Make sure we didn't get an error code then set the role
                     if(typeof(newName) == "string") {
-                        Memory.creeps[newName].role = creepTypes("list")[creepType];
+                        Memory.creeps[newName].role = typeName;
                     }
                 }
             } else {
