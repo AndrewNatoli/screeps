62df6e9b55e0148ce1a3840ec50213c526f6bf97
Andrew Natoli <andrew@desktopsolutions.com>
2014-11-26 22:51:08 -0500

    Fix #1 - Prioritize guards when under attack. Also don't spawn builders without having a guard and harvester first.

--- population.js
+++ population.js
@@ -7,36 +7,39 @@ module.exports = function() {
         
         var typeName = creepTypes("list")[creepType];
         
-        // Do we not have any of this type?
-        if(util.count(typeName) === 0) {
-            // Do we have a free spawner?
-            var spawn = util.spawn();
-            if(spawn !== undefined) {
-                // If we're under attack, prioritize guards
-                if(spawn.room.find(Game.HOSTILE_CREEPS).length > 0) {
-                    // If our forces are outnumbered...
-                    if(spawn.room.find(Game.HOSTILE_CREEPS).length > spawn.room.find(Game.MY_CREEPS).length) {
-                        // If we can afford a guard, spawn one. Otherwise spawn a harvester.
-                        if(spawn.energy > creepCosts.getCost(creepTypes(typeName))) {
-                            // Skip this unit if it's not a guard.
-                            if(typeName != "guard") {
+        // Do we have a free spawner?
+        var spawn = util.spawn();
+        if(spawn !== undefined) {
+            // If we're under attack, prioritize guards
+            if(spawn.room.find(Game.HOSTILE_CREEPS).length > 0) {
+                // If our forces are outnumbered...
+                if(spawn.room.find(Game.HOSTILE_CREEPS).length > spawn.room.find(Game.MY_CREEPS).length) {
+                    // If we can afford a guard, spawn one. Otherwise spawn a harvester.
+                    if(spawn.energy > creepCosts.getCost(creepTypes(typeName))) {
+                        // Skip this unit if it's not a guard.
+                        if(typeName != "guard") {
+                            continue;
+                        }
+                    } 
+                    // We can't afford a guard yet.
+                    else {
+                        // Do we need a harvester?
+                        if(util.count("harvester") === 0) {
+                            // Skip this unit if it's not a harvester
+                            if(typeName != "harvester") {
                                 continue;
-                            }
-                        } 
-                        // We can't afford a guard yet.
-                        else {
-                            // Do we need a harvester?
-                            if(util.count("harvester") === 0) {
-                                // Skip this unit if it's not a harvester
-                                if(typeName != "harvester") {
-                                    continue;
-                                }
                             }
                         }
                     }
                 }
-                
-                
+            }
+            
+            // Do we not have any of this type?
+            if(util.count(typeName) === 0) {
+                // Don't add builders if we don't have guards or harvesters.
+                if(typeName == "builder" && (util.count("harvester") == 0 || util.count("guard") == 0)) {
+                    continue;
+                }
                 // Can we afford this type?
                 if(spawn.energy > creepCosts.getCost(creepTypes(typeName))) {
                     var newName = spawn.createCreep(creepTypes(typeName));
@@ -45,8 +48,6 @@ module.exports = function() {
                         Memory.creeps[newName].role = typeName;
                     }
                 }
-            } else {
-                
             }
         }
     }
